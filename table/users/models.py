8358d6f7c8515a029ebf08feb077fe6cab5d2677
Дмитрий Свечкарь <svechkar.dmitry@yandex.ru>
2024-04-15 14:10:35 +0300

    fix user manager

--- table/users/models.py
+++ table/users/models.py
@@ -28,6 +28,7 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
         return self.create_user(email, password, **extra_fields)
 
 
